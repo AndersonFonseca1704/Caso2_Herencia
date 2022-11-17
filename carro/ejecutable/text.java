ccf735937b0f850159389e17e2cab570d4b58792
Anderson Andrés Fonseca Álvarez <102609948+AndersonFonseca1704@users.noreply.github.com>
2022-11-17 08:42:14 -0500

    Add files via upload

---
+++ carro/ejecutable/text.java
@@ -0,0 +1,9 @@
+package ejecutable;
+import modelo.Carro;
+public class text {
+     public static void main(String[]args)
+
+     // crear un objecto de tipo carro usando constructor sin parametros
+     System.out.print("color:" + carro1.getcolor());
+    
+}
